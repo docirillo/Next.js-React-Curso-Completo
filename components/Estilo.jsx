6f2062aefdb4336e8b27b56da0f0e224b1d9dfc7
Daniel Cirillo <sapremo@outlook.com>
2021-10-04 22:33:38 -0300

    Aplicando Estilo CSS

---
+++ components/Estilo.jsx
@@ -0,0 +1,15 @@
+export default function Estilo(props) {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: props.numero >= 0 ? '#2D2' : '#D22',
+          color: '#fff',
+          textAlign: props.direita ? 'right' : 'left',
+        }}
+      >
+        Texto Qualquer
+      </h1>
+    </div>
+  );
+}
