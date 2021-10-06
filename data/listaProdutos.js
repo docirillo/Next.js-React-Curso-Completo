76484087a131a4ced59abf7636519dd83d6b47ee
Daniel Cirillo <sapremo@outlook.com>
2021-10-05 21:31:27 -0300

    repetição com produtos

---
+++ data/listaProdutos.js
@@ -0,0 +1,8 @@
+import Produto from "../model/produto";
+
+export default [
+  new Produto(1, "iPad", 7543),
+  new Produto(2, "iPhone", 8000),
+  new Produto(3, "Xbox Series S", 2200),
+  new Produto(4, "Nintendo Switch", 2000),
+]
