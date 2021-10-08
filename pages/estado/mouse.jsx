d182595a50e57ad29a6935493b4339735f6c306e
Daniel Cirillo <sapremo@outlook.com>
2021-10-07 21:09:47 -0300

    modo 1

---
+++ pages/estado/mouse.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export default function Mouse() {
+  const arrayX = useState(0);
+  let x = arrayX[0];
+  const alterarX = arrayX[1];
+
+  const arrayY = useState(0);
+  let y = arrayY[0];
+  const alterarY = arrayY[1];
+
+  const estilo = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222',
+    color: '#fff',
+    height: '100vh',
+  };
+
+  function quandoMover(ev) {
+    alterarX(ev.clientX);
+    alterarY(ev.clientY);
+  }
+
+  return (
+    <div style={estilo} onMouseMove={quandoMover}>
+      <span>Eixo X: {x}</span>
+      <span>Eixo Y: {y}</span>
+    </div>
+  );
+}
